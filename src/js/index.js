b740014351de206612815e00f182e80d42d6f150
aleidagonzalez <aleida1983@hotmail.com>
2021-03-07 16:49:32 +0000

    cabios

--- src/js/index.js
+++ src/js/index.js
@@ -36,7 +36,8 @@ function render(variables = {}) {
           <h1>${variables.name}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city}</h3>
-          <ul class=" position-left">
+          <h4>${variables.country}</h4>
+          <ul class="position-left">${variables.socialMediaPosition}
             <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
             <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
             <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
